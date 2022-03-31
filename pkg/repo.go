4286c34e4a09449ac430a47a52b4f188ce02d2aa
yuribenelli <y.benelli@mind-informatica.com>
2022-03-31 15:39:53 +0200

    provafunzioni

--- pkg/repo.go
+++ pkg/repo.go
@@ -3,3 +3,10 @@ package repo
 func Somma(x int, y int) int {
 	return x + y
 }
+func Sottr(x int, y int) int {
+	return x - y
+}
+func MergeNamePass(name string, pass string) string {
+	z := name + pass
+	return z
+}
--- src/main.go
+++ src/main.go
@@ -4,7 +4,48 @@ import (
 	"fmt"
 )
 
-func main() {
-	fmt.Print("pippo")
+func (s user) merge() string {
+	return s.login + s.password
+}
 
+type user struct {
+	login    string
+	password string
+}
+type superUser struct {
+	user
+	hasPermit bool
+}
+
+type human interface {
+	merge()
+}
+
+func main() {
+	p1 := user{
+		login:    "yuribenelli",
+		password: "pippo",
+	}
+	p2 := user{
+		login:    "giovannigalli",
+		password: "pluto",
+	}
+	p3 := superUser{
+		user: user{
+			login:    "mariorossi",
+			password: "paperino",
+		},
+		hasPermit: true,
+	}
+	fmt.Println(p1)
+	fmt.Println(p2)
+	fmt.Println(p3)
+
+	fmt.Println("PERSON 1", p1.merge())
+	fmt.Println("PERSON 2", p2.merge())
+	fmt.Println("PERSON 3", p3.merge())
+
+	fmt.Printf("PERSON 1: \t%v\t%T\n", p1, p1)
+	fmt.Printf("PERSON 1: \t%v\t%T\n", p2, p2)
+	fmt.Printf("PERSON 1: \t%v\t%T\n", p3, p3)
 }
